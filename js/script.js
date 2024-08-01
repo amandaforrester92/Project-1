7dad10b6f33af4dd82b7234c9575a81211c41b70
Tinoreyes21 <tino_reyes@yahoo.com>
2024-07-31 20:49:40 -0700

    update. js notes

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 console.log('hello world! Project initialized')
+// this is notes
+// list of data for all consoles
+// for each console add navagation link with the console name
+// when we click left or right button then we change to current console
+// keep track of current console 
+// when we change the console set big images, small images, 
+// when the page is loaded, load first console
+// have javascript write
+// define an array for object  
