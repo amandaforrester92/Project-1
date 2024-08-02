81c3158a62308180d9a3ab123306726f09754c23
amanda forrester <a.soriano92@aol.com>
2024-08-01 19:12:25 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,3 +8,93 @@ console.log('hello world! Project initialized')
 // when the page is loaded, load first console
 // have javascript write
 // define an array for object  
+
+const playstationModels = [
+    // Tab 1
+    {
+        model: "Playstation (DISC)",
+        releaseDate: "December 1995",
+        originalPrice: 300,
+        memory: "N/A (Memory Card sold separately)"
+    },
+    {
+        model: "Playstation PS One (DISC)",
+        releaseDate: "September 2000",
+        originalPrice: 300,
+        memory: "N/A"
+    },
+
+    // Tab 2
+    {
+        model: "Playstation 2 (DISC)",
+        releaseDate: "October 2000",
+        originalPrice: 300,
+        memory: "8MB"
+    },
+    {
+        model: "Playstation 2 Slim (DISC)",
+        releaseDate: "November 2004",
+        originalPrice: 300,
+        memory: "1TB"
+    },
+
+    // Tab 3
+    {
+        model: "Playstation 3 (DISC)",
+        releaseDate: "2006",
+        originalPrice: 500,
+        memory: "20GB, 60GB"
+    },
+    {
+        model: "PS3 Slim (DISC)",
+        releaseDate: "2009",
+        originalPrice: 300,
+        memory: "1TB"
+    },
+    {
+        model: "PS3 Super Slim (DISC)",
+        releaseDate: "2012",
+        originalPrice: 300,
+        memory: "120GB, 160GB, 250GB, 320GB"
+    },
+
+    // Tab 4
+    {
+        model: "Playstation 4 (DISC&Digital)",
+        releaseDate: "2013",
+        originalPrice: 400,
+        memory: "500GB"
+    },
+    {
+        model: "Playstation 4 Slim",
+        releaseDate: "2016",
+        originalPrice: 300,
+        memory: "1TB"
+    },
+    {
+        model: "Playstation 4 PRO",
+        releaseDate: "2016",
+        originalPrice: 400,
+        memory: "825GB"
+    },
+
+    // Tab 5
+    {
+        model: "Playstation 5 (DISC&Digital)",
+        releaseDate: "November 2020",
+        originalPrice: 500,
+        memory: "825GB"
+    },
+    {
+        model: "Playstation 5 Digital Edition",
+        releaseDate: "November 2020",
+        originalPrice: 400,
+        memory: "825GB"
+    },
+    {
+        model: "Playstation 5 Slim (DISC)",
+        releaseDate: "November 2023",
+        originalPrice: 450,
+        memory: "1TB"
+    }
+];
